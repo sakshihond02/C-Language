c35a747d111aee0e3f74fdfeaec0d808ca84d603
sakshihond02 <sakshihond@gmail.com>
2025-12-09 19:44:56 +0530

    Add files via upload

---
+++ positive_negative.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter Number :");
+	scanf("%d",&num);
+	if(num>0)
+	   printf("Number is positive");
+	else
+	   printf("Number is Negative");
+}
+	   
