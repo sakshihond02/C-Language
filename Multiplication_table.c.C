d87af88af71d05c2f6c42ecc341f21d2fe3f40b6
sakshihond02 <sakshihond@gmail.com>
2025-12-09 19:32:16 +0530

    Add files via upload

---
+++ Multiplication_table.c.C
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main() 
+{ 	
+    int a,i; 	
+    printf("Enter Number :"); 	
+    scanf("%d",&a); 	
+    for(i=1; i<=10; i++) 	
+    { 		
+        printf("/n %d",a*i);
+	
+    } 
+}
+
